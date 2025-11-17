e9ac554d2cd2aa575240561d6bb702e0808c911f
Juan Villanueva <jcarlosv2002@gmail.com>
2025-11-17 11:40:49 -0600

    adding imports and beginning with class particle

---
+++ Galaxy.py
@@ -0,0 +1,19 @@
+#galaxy color shift 
+#galaxy class 
+#particle class
+#main 
+
+import random 
+import pygame 
+import math 
+
+#class particle: represents a single star in the galaxy
+class Particle(): 
+    def __init(self, size=4, life=3000, palette=None): 
+        self.center = center 
+        self.size = size
+        self.life = life
+        self.age = 0 
+        self.dead = False
+        self.aplha = 255
+
